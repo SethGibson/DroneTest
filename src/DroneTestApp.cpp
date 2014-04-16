01d96aee49f54eb240d279ff847ecf63ee057d0f
Seth Gibson <seth.gibson@intel.com>
2014-04-16 13:34:53 -0700

    initial commit -sG

--- src/DroneTestApp.cpp
+++ src/DroneTestApp.cpp
@@ -20,21 +20,24 @@ public:
 	void mouseDown( MouseEvent event );	
 	void update();
 	void updateCamera();
-	void updateHand(PXCGesture::GeoNode::Label pHand, PXCHand &pHandData);
-	void updateOSC();
-	void onButtonDn();
+	//void updateHand(PXCGesture::GeoNode::Label pHand, vector<Vec3f> &pHandData);
+	//void updateOSC();
+	void onButtonOscDn();
+	void onButtonCamDn();
 	void draw();
-
+	void quit();
 
 private:
+	vector<Vec3f> mHand0Pos;
+	vector<Vec3f> mHand1Pos;
 	gl::Texture mBlobTex;
 	PXCHand mHand0;
 	PXCHand mHand1;
 
 	params::InterfaceGl mGUI;
-	int32_t mPortParam;
-	string mIpParam, mStatusParam;
-	bool mSending;
+	int32_t mParamPort;
+	string mParamIp, mParamOscStatus, mParamCamStatus;
+	bool mParamOscOnOff, mParamCamOnOff, mParamFps60;
 
 	osc::Sender mSender;
 
@@ -43,7 +46,7 @@ private:
 };
 void DroneTestApp::prepareSettings(Settings *pSettings)
 {
-	pSettings->setWindowSize(568,272);
+	pSettings->setWindowSize(900,510);
 	pSettings->setFrameRate(30);
 }
 
@@ -51,38 +54,43 @@ void DroneTestApp::setup()
 {
 	setupGUI();
 	setupPXC();
-	setupOSC();
+	//setupOSC();
 }
 
 void DroneTestApp::setupGUI()
 {
-	mPortParam = 2710;
-	mIpParam = "192.168.1.255";
-	mSending = false;
-	mStatusParam = "Not Sending";
+	mParamPort = 2710;
+	mParamIp = "192.168.1.255";
+	mParamOscOnOff = false;
+	mParamCamOnOff = false;
+	mParamFps60 = false;
+	mParamOscStatus = "Not Sending";
+	mParamCamStatus = "Disabled";
 	
-	mGUI = params::InterfaceGl("OSC Test", Vec2i(200,240), ColorA(0.05f,0.4f,0.65f,0.5f));
-	mGUI.setOptions("", "valueswidth=110");	
+	mGUI = params::InterfaceGl("Hand Position OSC", Vec2i(200,300), ColorA(0.05f,0.4f,0.65f,0.5f));
+	mGUI.setOptions("", "valueswidth=80 position='20 15'");	
 
 	mGUI.addSeparator();
-	mGUI.addParam("Ip", &mIpParam);
-	mGUI.addParam("Port", &mPortParam);
+	mGUI.addParam("60 Fps", &mParamFps60);
+	mGUI.addButton("Start/Stop Camera", std::bind(&DroneTestApp::onButtonCamDn, this)); 
+	mGUI.addParam("Camera Status: ", &mParamCamStatus);
 	mGUI.addSeparator();
-	mGUI.addButton("Start/Stop", std::bind(&DroneTestApp::onButtonDn, this)); 
+	mGUI.addParam("Ip", &mParamIp);
+	mGUI.addParam("Port", &mParamPort);
+	mGUI.addButton("Start/Stop OSC", std::bind(&DroneTestApp::onButtonOscDn, this)); 
+	mGUI.addParam("OSC Status: ", &mParamOscStatus);
 	mGUI.addSeparator();
-	mGUI.addParam("Status: ", &mStatusParam);
 }
 
 void DroneTestApp::setupPXC()
 {
-	mPXC.EnableImage(PXCImage::COLOR_FORMAT_DEPTH);
 	mPXC.EnableGesture();
 	mPXC.Init();
 }
 
 void DroneTestApp::setupOSC()
 {
-	mSender.setup(mIpParam, mPortParam, true);
+	mSender.setup(mParamIp, mParamPort, true);
 }
 
 void DroneTestApp::mouseDown( MouseEvent event )
@@ -91,9 +99,14 @@ void DroneTestApp::mouseDown( MouseEvent event )
 
 void DroneTestApp::update()
 {
-	updateCamera();
-	if(mSending)
-		updateOSC();
+	if(mParamCamOnOff)
+	{
+		updateCamera();
+		/*
+		if(mSending)
+			updateOSC();
+		*/
+	}
 }
 
 void DroneTestApp::updateCamera()
@@ -111,13 +124,13 @@ void DroneTestApp::updateCamera()
 				cBlob->ReleaseAccess(&cBlobData);
 			}
 		}
-		updateHand(PXCGesture::GeoNode::LABEL_BODY_HAND_PRIMARY, mHand0);	
-		updateHand(PXCGesture::GeoNode::LABEL_BODY_HAND_SECONDARY, mHand1);	
+		//updateHand(PXCGesture::GeoNode::LABEL_BODY_HAND_PRIMARY, mHand0);	
+		//updateHand(PXCGesture::GeoNode::LABEL_BODY_HAND_SECONDARY, mHand1);	
 		mPXC.ReleaseFrame();
 	}
 }
-
-void DroneTestApp::updateHand(PXCGesture::GeoNode::Label pHand, PXCHand &pHandData)
+/*
+void DroneTestApp::updateHand(PXCGesture::GeoNode::Label pHand, vector<Vec3f> &pHandData)
 {
 
 	PXCGesture::GeoNode cHandNode;
@@ -147,112 +160,61 @@ void DroneTestApp::updateHand(PXCGesture::GeoNode::Label pHand, PXCHand &pHandDa
 		pHandData.setNode(5,Vec2f(cHandNode.positionImage.x, cHandNode.positionImage.y), Vec3f(cHandNode.positionWorld.x,cHandNode.positionWorld.z, cHandNode.positionWorld.y));
 		pHandData.HasPinky = true;	}
 }
+*/
 
+/*
 void DroneTestApp::updateOSC()
 {
-	osc::Bundle hand0Bundle, hand1Bundle;
+	osc::Bundle handBundle;
+
 	if(mHand0.IsActive())
 	{
-		osc::Message palm, thumb, index, middle, ring, pinky;
-		palm.setAddress("/hand0/palm/");
-		palm.addIntArg((int)mHand0.HasPalm);
-		palm.addFloatArg(mHand0.PalmNode.ImagePos.x);palm.addFloatArg(mHand0.PalmNode.ImagePos.y);
-		palm.addFloatArg(mHand0.PalmNode.WorldPos.x);palm.addFloatArg(mHand0.PalmNode.WorldPos.y);palm.addFloatArg(mHand0.PalmNode.WorldPos.z);
-		palm.setRemoteEndpoint(mIpParam, mPortParam);
-		hand0Bundle.addMessage(palm);
-
-		thumb.setAddress("/hand0/thumb/");
-		thumb.addIntArg((int)mHand0.HasThumb);
-		thumb.addFloatArg(mHand0.ThumbNode.ImagePos.x);thumb.addFloatArg(mHand0.ThumbNode.ImagePos.y);
-		thumb.addFloatArg(mHand0.ThumbNode.WorldPos.x);thumb.addFloatArg(mHand0.ThumbNode.WorldPos.y);thumb.addFloatArg(mHand0.ThumbNode.WorldPos.z);
-		thumb.setRemoteEndpoint(mIpParam, mPortParam);
-		hand0Bundle.addMessage(thumb);
-
-		index.setAddress("/hand0/index/");
-		index.addIntArg((int)mHand0.HasIndex);
-		index.addFloatArg(mHand0.IndexNode.ImagePos.x);index.addFloatArg(mHand0.IndexNode.ImagePos.y);
-		index.addFloatArg(mHand0.IndexNode.WorldPos.x);index.addFloatArg(mHand0.IndexNode.WorldPos.y);index.addFloatArg(mHand0.IndexNode.WorldPos.z);
-		index.setRemoteEndpoint(mIpParam, mPortParam);
-		hand0Bundle.addMessage(index);
-
-		middle.setAddress("/hand0/middle/");
-		middle.addIntArg((int)mHand0.HasMiddle);
-		middle.addFloatArg(mHand0.MiddleNode.ImagePos.x);middle.addFloatArg(mHand0.MiddleNode.ImagePos.y);
-		middle.addFloatArg(mHand0.MiddleNode.WorldPos.x);middle.addFloatArg(mHand0.MiddleNode.WorldPos.y);middle.addFloatArg(mHand0.MiddleNode.WorldPos.z);
-		middle.setRemoteEndpoint(mIpParam, mPortParam);
-		hand0Bundle.addMessage(middle);
-
-		ring.setAddress("/hand0/ring/");
-		ring.addIntArg((int)mHand0.HasRing);
-		ring.addFloatArg(mHand0.RingNode.ImagePos.x);ring.addFloatArg(mHand0.RingNode.ImagePos.y);
-		ring.addFloatArg(mHand0.RingNode.WorldPos.x);ring.addFloatArg(mHand0.RingNode.WorldPos.y);ring.addFloatArg(mHand0.RingNode.WorldPos.z);
-		ring.setRemoteEndpoint(mIpParam, mPortParam);
-		hand0Bundle.addMessage(ring);
-
-		pinky.setAddress("/hand0/pinky/");
-		pinky.addIntArg((int)mHand0.HasPinky);
-		pinky.addFloatArg(mHand0.PinkyNode.ImagePos.x);pinky.addFloatArg(mHand0.PinkyNode.ImagePos.y);
-		pinky.addFloatArg(mHand0.PinkyNode.WorldPos.x);pinky.addFloatArg(mHand0.PinkyNode.WorldPos.y);pinky.addFloatArg(mHand0.PinkyNode.WorldPos.z);
-		pinky.setRemoteEndpoint(mIpParam, mPortParam);
-		hand0Bundle.addMessage(pinky);
+		osc::Message hand0;
+		hand0.setAddress("/hand0/");
+		hand0.addIntArg((int)mHand0.HasPalm);
+		hand0.addFloatArg(mHand0.PalmNode.ImagePos.x);palm.addFloatArg(mHand0.PalmNode.ImagePos.y);
+		hand0.addFloatArg(mHand0.PalmNode.WorldPos.x);palm.addFloatArg(mHand0.PalmNode.WorldPos.y);palm.addFloatArg(mHand0.PalmNode.WorldPos.z);
+		hand0.setRemoteEndpoint(mIpParam, mPortParam);
+		handBundle.addMessage(hand0);
 	}
 
 	if(mHand1.IsActive())
 	{
-		osc::Message palm, thumb, index, middle, ring, pinky;
-		palm.setAddress("/hand1/palm/");
-		palm.addIntArg((int)mHand1.HasPalm);
-		palm.addFloatArg(mHand1.PalmNode.ImagePos.x);palm.addFloatArg(mHand1.PalmNode.ImagePos.y);
-		palm.addFloatArg(mHand1.PalmNode.WorldPos.x);palm.addFloatArg(mHand1.PalmNode.WorldPos.y);palm.addFloatArg(mHand1.PalmNode.WorldPos.z);
-		palm.setRemoteEndpoint(mIpParam, mPortParam);
-		hand1Bundle.addMessage(palm);
-
-		thumb.setAddress("/hand1/thumb/");
-		thumb.addIntArg((int)mHand1.HasThumb);
-		thumb.addFloatArg(mHand1.ThumbNode.ImagePos.x);thumb.addFloatArg(mHand1.ThumbNode.ImagePos.y);
-		thumb.addFloatArg(mHand1.ThumbNode.WorldPos.x);thumb.addFloatArg(mHand1.ThumbNode.WorldPos.y);thumb.addFloatArg(mHand1.ThumbNode.WorldPos.z);
-		thumb.setRemoteEndpoint(mIpParam, mPortParam);
-		hand1Bundle.addMessage(thumb);
-
-		index.setAddress("/hand1/index/");
-		index.addIntArg((int)mHand1.HasIndex);
-		index.addFloatArg(mHand1.IndexNode.ImagePos.x);index.addFloatArg(mHand1.IndexNode.ImagePos.y);
-		index.addFloatArg(mHand1.IndexNode.WorldPos.x);index.addFloatArg(mHand1.IndexNode.WorldPos.y);index.addFloatArg(mHand1.IndexNode.WorldPos.z);
-		index.setRemoteEndpoint(mIpParam, mPortParam);
-		hand1Bundle.addMessage(index);
-
-		middle.setAddress("/hand1/middle/");
-		middle.addIntArg((int)mHand1.HasMiddle);
-		middle.addFloatArg(mHand1.MiddleNode.ImagePos.x);middle.addFloatArg(mHand1.MiddleNode.ImagePos.y);
-		middle.addFloatArg(mHand1.MiddleNode.WorldPos.x);middle.addFloatArg(mHand1.MiddleNode.WorldPos.y);middle.addFloatArg(mHand1.MiddleNode.WorldPos.z);
-		middle.setRemoteEndpoint(mIpParam, mPortParam);
-		hand1Bundle.addMessage(middle);
-
-		ring.setAddress("/hand1/ring/");
-		ring.addIntArg((int)mHand1.HasRing);
-		ring.addFloatArg(mHand1.RingNode.ImagePos.x);ring.addFloatArg(mHand1.RingNode.ImagePos.y);
-		ring.addFloatArg(mHand1.RingNode.WorldPos.x);ring.addFloatArg(mHand1.RingNode.WorldPos.y);ring.addFloatArg(mHand1.RingNode.WorldPos.z);
-		ring.setRemoteEndpoint(mIpParam, mPortParam);
-		hand1Bundle.addMessage(ring);
-
-		pinky.setAddress("/hand1/pinky/");
-		pinky.addIntArg((int)mHand1.HasPinky);
-		pinky.addFloatArg(mHand1.PinkyNode.ImagePos.x);pinky.addFloatArg(mHand1.PinkyNode.ImagePos.y);
-		pinky.addFloatArg(mHand1.PinkyNode.WorldPos.x);pinky.addFloatArg(mHand1.PinkyNode.WorldPos.y);pinky.addFloatArg(mHand1.PinkyNode.WorldPos.z);
-		pinky.setRemoteEndpoint(mIpParam, mPortParam);
-		hand1Bundle.addMessage(pinky);
+		osc::Message hand1;
+		hand1.setAddress("/hand1/");
+		hand1.addIntArg((int)mHand1.HasPalm);
+		hand1.addFloatArg(mHand1.PalmNode.ImagePos.x);palm.addFloatArg(mHand1.PalmNode.ImagePos.y);
+		hand1.addFloatArg(mHand1.PalmNode.WorldPos.x);palm.addFloatArg(mHand1.PalmNode.WorldPos.y);palm.addFloatArg(mHand1.PalmNode.WorldPos.z);
+		hand1.setRemoteEndpoint(mIpParam, mPortParam);
+		handBundle.addMessage(hand1);
 	}
 
-	mSender.sendBundle(hand0Bundle);
-	mSender.sendBundle(hand1Bundle);
+	mSender.sendBundle(handBundle);
+}
+*/
+void DroneTestApp::onButtonOscDn()
+{
+	mParamOscOnOff=!mParamOscOnOff;
+	mParamOscStatus = mParamOscOnOff?"Sending":"Not Sending";
+	if(mParamOscOnOff)
+		mSender.setup(mParamIp, mParamPort, true);
 }
 
-void DroneTestApp::onButtonDn()
+void DroneTestApp::onButtonCamDn()
 {
-	mSending=!mSending;
-	mStatusParam = mSending?"Sending":"Not Sending";
-	if(mSending)
-		mSender.setup(mIpParam, mPortParam, true);
+	mParamCamOnOff=!mParamCamOnOff;
+	mParamCamStatus = mParamOscOnOff?"Enabled":"Disabled";
+	if(mParamCamOnOff)
+	{
+		mPXC = UtilPipeline();
+		pxcU32 cFps = mParamFps60?60:30;
+		PXCSizeU32 cDepthSize;cDepthSize.width=320;cDepthSize.height=240;
+		mPXC.EnableImage(PXCImage::COLOR_FORMAT_DEPTH);
+		mPXC.QueryCapture()->SetFilter(PXCImage::IMAGE_TYPE_DEPTH, cDepthSize, cFps);
+		mPXC.Init();
+	}
+	else
+		mPXC.Close();
 }
 
 void DroneTestApp::draw()
@@ -262,11 +224,24 @@ void DroneTestApp::draw()
 
 	mGUI.draw();
 	gl::pushMatrices();
-	gl::translate(Vec2f(232,16));
-	gl::draw(mBlobTex,Vec2f::zero());
-	mHand0.draw(1.0f,Vec2f::zero());
-	mHand1.draw(1.0f,Vec2f::zero());
+	gl::translate(Vec2f(240,15));
+	if(mParamCamOnOff)
+	{
+		gl::color(Color::white());
+		gl::draw(mBlobTex,Rectf(0,0,640,480));
+	}
+	else
+	{
+		gl::color(Color(1,0,0));
+		gl::drawStrokedRect(Rectf(0,0,640,480));
+	}
 	gl::popMatrices();
 }
+
+void DroneTestApp::quit()
+{
+	mPXC.Close();
+}
+
 
 CINDER_APP_NATIVE( DroneTestApp, RendererGl )
